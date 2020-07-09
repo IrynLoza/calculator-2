b90185236399795fc8c14a0052670a58622faea7
Iryna Loza <irynloza@gmail.com>
2020-07-08 20:32:06 -0700

    Refuctoring the code and put inside the function

--- calculator.py
+++ calculator.py
@@ -5,49 +5,42 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+def	tokenization(input_string):
 
-while True:
-
-	input_string = input('Enter your equation > ')
-		
 	tokens = input_string.split(' ')
 
 	if tokens[0] == 'q' or tokens[0] == 'quit':
+		return 'END'
+
+	if 1 < len(tokens): # is index 1 exist in list
+		if tokens[0] == 'square':
+			print(square(int(tokens[1])))
+
+		elif tokens[0] == 'cube':
+			print(cube(int(tokens[1])))	
+
+	if 2 < len(tokens):	# is index 2 exist in list
+		if tokens[0] == '+':
+			print(add(int(tokens[1]), int(tokens[2])))
+
+		elif tokens[0] == '-':
+			print(subtract(int(tokens[1]), int(tokens[2])))
+
+		elif tokens[0] == '*':
+			print(multiply(int(tokens[1]), int(tokens[2])))
+
+		elif tokens[0] == '/':
+			print(divide(int(tokens[1]), int(tokens[2]))) 			
+
+		elif tokens[0] == 'pow':
+			print(power(int(tokens[1]), int(tokens[2])))
+
+		elif tokens[0] == 'mod':
+			print(mod(int(tokens[1]), int(tokens[2])))
+
+
+while True:
+	exit = tokenization(input('Enter your equation > '))
+	if exit == 'END':
 		break
-
-	if len(tokens) > 3:
-		tokens[0] = tokens[0]
-		num1 = tokens[1]
-		num2 = tokens[2]
-
-	if len(tokens) < 3:
-		tokens[0] = tokens[0]
-		num1 = tokens[1]	
-
-	if tokens[0] == '+':
-		print(add(int(tokens[1]), int(tokens[2])))
-
-	elif tokens[0] == '-':
-		print(subtract(int(tokens[1]), int(tokens[2])))
-
-	elif tokens[0] == '*':
-		print(multiply(int(tokens[1]), int(tokens[2])))
-
-	elif tokens[0] == '/':
-		print(divide(int(tokens[1]), int(tokens[2])))
-
-	elif tokens[0] == 'square':
-		print(square(int(tokens[1])))
-
-	elif tokens[0] == 'cube':
-		print(cube(int(tokens[1])))		 			
-
-	elif tokens[0] == 'pow':
-		print(power(int(tokens[1]), int(tokens[2])))
-
-	elif tokens[0] == 'mod':
-		print(mod(int(tokens[1]), int([tokens[2]])))
-
-
-
 
